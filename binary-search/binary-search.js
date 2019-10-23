f33fed178b7d681f3da798610f991adf7d917a63
Kathleen McDaniel <kathleen_mc.daniel@yahoo.com>
2019-10-23 16:27:22 -0400

    Binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,20 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (arr, target) => {
+	let midLength = Math.floor(arr.length / 2);
+	let midPoint = arr[midLength];
+	if (target === midPoint) {
+		console.log("I was found!", target)
+		return true;
+	} else if (target < midPoint) {
+		return binarySearch(arr.slice(0, midLength), target);
+	} else if (target > midPoint) {
+		return binarySearch(arr.slice(midLength + 1), target);
+	} else {
+		console.log("I was not found :(", target);
+		return false;
+	}
 };
 
 /*
